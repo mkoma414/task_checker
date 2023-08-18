72604bb08ff030b277f57a01ec2aaa6f060bcaed
Komatsubara Masato <m-koma@nifty.com>
2023-08-18 18:34:27 +0900

    トップページのマークアップ完成

---
+++ src/components/toDoList/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Task } from "../task";
+import { FormModal } from "../modal";
+import MenuIcon from "@material-ui/icons/Menu";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import "./style.css";
+
+export const ToDoList = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div className="task_list">
+      <div className="section">
+        <MenuIcon className="section_ele" />
+        <span className="section_ele">ToDo</span>
+        <AddCircleOutlineIcon
+          className="add_circle_outline_icon"
+          fontSize="small"
+          onClick={handleOpen}
+        />
+        <FormModal
+          handleClose={handleClose}
+          isOpen={isOpen}
+          body="taskBody"
+        />
+      </div>
+      <div className="task_field">
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Header } from "../../components/header";
+import { Select } from "../../components/select";
+import { ToDoList } from "../../components/toDoList";
+import { FormModal } from "../../components/modal";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import "./style.css";
+
+export const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="main">
+      <Header />
+      <div className="genre">
+        <Select />
+        <AddCircleOutlineIcon
+          className="add_circle_outline_icon"
+          fontSize="default"
+          onClick={handleOpen}
+        />
+        <FormModal
+          handleClose={handleClose}
+          isOpen={isOpen}
+          body="genreBody"
+        />
+      </div>
+      <div className="contents">
+        <ToDoList />
+      </div>
+    </div>
+  );
+};
