be8b96a2f29140e770dc01164ed833d4a080c2b0
Komatsubara Masato <m-koma@nifty.com>
2023-08-21 09:31:36 +0900

    サーバと通信しデータを表示しよう(前半)まで完了

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../../components/header";
 import { Select } from "../../components/select";
 import { ToDoList } from "../../components/toDoList";
 import { FormModal } from "../../components/modal";
+import { taskRequest } from "../../requests/taskRequest";
+import { genreRequest } from "../../requests/genreRequest";
+import { useDataReducer } from "../../hooks/useDataReducer";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import "./style.css";
 
 export const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [data, dispatch] = useDataReducer();
   const handleOpen = () => {
     setIsOpen(true);
   };
   const handleClose = () => {
     setIsOpen(false);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const genres = await genreRequest("fetchGenres");
+      const tasks = await taskRequest("fetchTasks");
+      dispatch({ type: "genresUpdate", payload: { genre: genres } });
+      dispatch({ type: "tasksUpdate", payload: { task: tasks } });
+    };
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <div className="main">
@@ -22,7 +41,7 @@ export const Home = () => {
         <Select />
         <AddCircleOutlineIcon
           className="add_circle_outline_icon"
-          fontSize="default"
+          fontSize="medium"
           onClick={handleOpen}
         />
         <FormModal
